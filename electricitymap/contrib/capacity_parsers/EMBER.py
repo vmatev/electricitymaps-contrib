cdd7f1e6985b472fd1b2949cde3bc173dd7ea2ee
Mathilde Daugy <mathilde.daugy@electricitymaps.com>
2023-10-02 10:25:03 +0200

    new capacity folder

---
+++ electricitymap/contrib/capacity_parsers/EMBER.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+EMBER_CSV_LINK = 'https://ember-climate.org/app/uploads/2022/07/yearly_full_release_long_format.csv'
+def fetch_capacities(link:str) -> pd.DataFrame:
+    """
+    Fetches the capacities from the EMBER dataset.
+    """
+    df = pd.read_csv(link)
+
+
+if __name__ == '__main__':
+    fetch_capacities(EMBER_CSV_LINK)
